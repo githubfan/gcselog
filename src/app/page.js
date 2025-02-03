a203657bf21f9281ade547a3b65b5bb2ab5910af
Josh Kalu <joshkalu18@gmail.com>
2025-02-03 11:22:47 +0000

    Styling Text, and adding background

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,11 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <html lang="en">
-      <h1 className="header-title">Built by Students, for Students</h1>
-      <p className="header-desc">A collaborative resource created by students, for students, across the country. By promoting open-source materials, we aim to provide students from all backgrounds with the tools they need to achieve their highest potential.</p>
-    </html>
-   
+
+    <header className="homepage-header">
+        <h1 className="header-title">Built by Students, for Students</h1>
+        <p className="header-desc">A collaborative resource created by students, for students, across the country. By promoting open-source materials, we aim to provide students from all backgrounds with the tools they need to achieve their highest potential.</p>
+      </header>
+         
   );
 }
