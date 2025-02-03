59c28e772e247ea3126ea75e0eeae91af9c3a609
Josh Kalu <joshkalu18@gmail.com>
2025-02-03 11:37:19 +0000

    Removing some unnecessary files, and adding in a (very) basic navbar

---
+++ src/app/components/navigation/navbar.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Styles from "./navbar-styling.css"
+const Navbar = () => {
+    return (
+    <nav className="navigation">
+        <h1>GCSE Log</h1>
+    </nav>
+
+    )
+}
+    
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
-import styles from "./page.module.css";
-
+import styles from "./globals.css";
+import Navbar from "./components/navigation/navbar"
 export default function Home() {
   return (
+    <div className="main-div">
 
+      <Navbar />
     <header className="homepage-header">
         <h1 className="header-title">Built by Students, for Students</h1>
         <p className="header-desc">A collaborative resource created by students, for students, across the country. By promoting open-source materials, we aim to provide students from all backgrounds with the tools they need to achieve their highest potential.</p>
       </header>
-         
+      </div>  
   );
 }
