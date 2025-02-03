98108fb5b149e14ade6ffe2da330f307365a1580
Josh Kalu <joshkalu18@gmail.com>
2025-02-03 12:20:37 +0000

    Add Navbar - no styling

--- src/app/components/navigation/navbar.js
+++ src/app/components/navigation/navbar.js
@@ -1,13 +1,31 @@
 import Link from "next/link";
-import Styles from "./navbar-styling.css"
+import "./navbar-styling.css"
 import Image from "next/image";
 const Navbar = () => {
     return (
-    <nav className="navigation">
-              <img className="logo" src="GCSE Log Logo.png" alt="GCSE Log Logo"  />
-    </nav>
+        <nav className="navigation">
+            <img className="logo" src="GCSE Log Logo.png" alt="GCSE Log Logo" />
+            <div className="menu-items">
+
+                <Link className="link" href="/">
+                    Subjects
+                </Link>
+
+                <Link className="link" href="/">
+                    Submit Resources
+                </Link>
+
+                <Link className="link" href="/">
+                    Blog
+                </Link>
+
+                <Link className="link" href="/">
+                    Other Opportunities
+                </Link>
+            </div>
+        </nav>
 
     )
 }
-    
+
 export default Navbar;
