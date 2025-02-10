28b6a7cab010ef17be4831d61d0292d55220c08b
Josh Kalu <joshkalu18@gmail.com>
2025-02-10 21:25:17 +0000

    Making both the Navbar, and the header responsive

--- src/app/components/navigation/navbar.js
+++ src/app/components/navigation/navbar.js
@@ -1,27 +1,42 @@
+'use client'
+
+import { useState } from "react";
 import Link from "next/link";
 import "./navbar-styling.css"
 import Image from "next/image";
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className="navigation">
             <img className="logo" src="GCSE Log Logo.png" alt="GCSE Log Logo" />
-            <div className="menu-items">
-                <Link className="link" href="/">
+
+            <div className={`menu-items ${menuOpen ? "active" : ""}`}>
+                <Link className="link" href="/" onClick={() => setMenuOpen(false)}>
                     Browse Subjects
                 </Link>
-
-                <Link className="link" href="/">
+                <Link className="link" href="/" onClick={() => setMenuOpen(false)}>
                     Submit Resources
                 </Link>
-
-                <Link className="link" href="/">
+                <Link className="link" href="/" onClick={() => setMenuOpen(false)}>
                     Blog
                 </Link>
-                <Link className="link" href="/">
+                <Link className="link" href="/" onClick={() => setMenuOpen(false)}>
                     Other Opportunities
                 </Link>
-            <button className="turqBtn">Access it Now!</button>
+                <button className="turqBtn" onClick={() => setMenuOpen(false)}>Access it Now!</button>
             </div>
+
+            <div className={`hamburger ${menuOpen ? "active" : ""}`} onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
         </nav>
 
     )
