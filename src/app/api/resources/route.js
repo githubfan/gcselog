80fce6791d127e354bf55841453c3f4cb189ec8a
Josh Kalu <joshkalu18@gmail.com>
2025-04-09 14:39:37 +0100

    Sorting resources by showing those with the highest rating first

--- src/app/api/resources/route.js
+++ src/app/api/resources/route.js
@@ -2,6 +2,12 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const resources = await prisma.resource.findMany();
+  const resources = await prisma.resource.findMany( // fetching resources by average rating 
+    {
+      orderBy: {
+        averageRating: 'desc',
+      }
+    }
+  );
   return Response.json(resources);
 }
